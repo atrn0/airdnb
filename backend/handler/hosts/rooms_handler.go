4f661393a37a2db600b733524282187d982f4672
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-11 18:43:16 +0900

    impl update rooms

--- backend/handler/hosts/rooms_handler.go
+++ backend/handler/hosts/rooms_handler.go
@@ -20,6 +20,7 @@ type RoomsHandlerImpl struct {
 type HostsRoomsHandler interface {
 	HostsGetRooms(ctx echo.Context) error
 	HostsPostRooms(ctx echo.Context) error
+	HostsPutRooms(ctx echo.Context, roomId string) error
 	HostsDeleteRooms(ctx echo.Context, roomId string) error
 }
 
@@ -90,6 +91,67 @@ func (h *RoomsHandlerImpl) HostsPostRooms(ctx echo.Context) error {
 	VALUES (:id, :name, :price, :host_id) 
 	`, roomCreate)
 
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError,
+			errors.ErrorRes{Message: err.Error()})
+	}
+
+	return ctx.NoContent(http.StatusNoContent)
+}
+
+func (h *RoomsHandlerImpl) HostsPutRooms(ctx echo.Context, roomId string) error {
+	userId := auth.GetUserId(ctx)
+	if userId == "" {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			errors.ErrorRes{Message: "userId is required"},
+		)
+	}
+
+	tx := h.db.MustBegin()
+
+	var room entity.Room
+	err := tx.Get(&room, `
+		SELECT * from rooms where id = $1
+	`, roomId)
+	if err != nil {
+		return ctx.JSON(http.StatusNotFound,
+			errors.ErrorRes{Message: "the room not found"})
+	}
+
+	if room.HostID != userId {
+		return ctx.JSON(http.StatusUnauthorized, errors.ErrorRes{Message: "not authorized"})
+	}
+
+	req := new(oapi.HostsPutRoomsReq)
+	if err := ctx.Bind(req); err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			errors.ErrorRes{Message: err.Error()},
+		)
+	}
+
+	if req.Name != "" {
+		_, err = tx.Exec(`
+		UPDATE rooms SET name = $1 WHERE id = $3
+	`, req.Name, roomId)
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError,
+				errors.ErrorRes{Message: err.Error()})
+		}
+	}
+
+	if req.Price > 0 {
+		_, err = tx.Exec(`
+		UPDATE rooms SET price = $1 WHERE id = $3
+	`, req.Price, roomId)
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError,
+				errors.ErrorRes{Message: err.Error()})
+		}
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		return ctx.JSON(http.StatusInternalServerError,
 			errors.ErrorRes{Message: err.Error()})
